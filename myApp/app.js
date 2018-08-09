3ed5799dfaa3ba0115d7d2b222497ea48c2866a5
Cuong <Cuong@gmail.com>
2018-08-09 17:31:40 +0700

    socketcluster

---
+++ myApp/app.js
@@ -0,0 +1,8 @@
+var SCWorker = require('socketcluster/scworker');
+var express = require('express');
+var serveStatic = require('serve-static');
+var path = require('path');
+var morgan = require('morgan');
+var healthChecker = require('sc-framework-health-check');
+
+
